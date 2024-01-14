b7d338c64dc9b7f10a4f80be2d086d11b51af169
Marouane <6729798+MarouaneMan@users.noreply.github.com>
2024-01-14 17:06:00 +0100

    feat: repos handler

---
+++ internal/restservice/repos_handler.go
@@ -0,0 +1,41 @@
+package restservice
+
+import (
+	"encoding/json"
+	"github.com/MarouaneMan/github-api/api"
+	"github.com/MarouaneMan/github-api/kvstore"
+	"github.com/Scalingo/go-utils/logger"
+	"net/http"
+)
+
+// ReposHandler returns n latest git repositories
+func ReposHandler(storeReader kvstore.Reader) func(http.ResponseWriter, *http.Request, map[string]string) error {
+	return func(w http.ResponseWriter, r *http.Request, params map[string]string) error {
+		log := logger.Get(r.Context())
+
+		queryParams := r.URL.Query()
+
+		repositories, ok := storeReader.Read(r.Context(), "repositories").([]*api.Repository)
+		if !ok {
+			repositories = []*api.Repository{}
+		}
+
+		var filteredRepositories = FilterRepositories(
+			NewFilterConfig(
+				WithLanguage(queryParams.Get("language")),
+				WithOwner(queryParams.Get("owner")),
+				WithLimit(queryParams.Get("limit")),
+				// WithLicense(queryParams.Get("license")), // not provided in /repositories
+			),
+			repositories,
+		)
+
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		err := json.NewEncoder(w).Encode(filteredRepositories)
+		if err != nil {
+			log.WithError(err).Error("Fail to encode JSON")
+		}
+		return nil
+	}
+}
---
+++ internal/restservice/repos_handler_test.go
@@ -0,0 +1,100 @@
+package restservice
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/MarouaneMan/github-api/api"
+	"github.com/MarouaneMan/github-api/kvstore"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+)
+
+var repos = []*api.Repository{
+	{
+		Repository: "repo1",
+		Owner:      "owner1",
+		Languages: map[string]api.Language{
+			"golang": {
+				Bytes: 1234,
+			},
+		},
+	},
+	{
+		Repository: "repo2",
+		Owner:      "owner2",
+		Languages: map[string]api.Language{
+			"c++": {
+				Bytes: 1234,
+			},
+		},
+	},
+	{
+		Repository: "repo3",
+		Owner:      "owner3",
+		Languages: map[string]api.Language{
+			"golang": {
+				Bytes: 1234,
+			},
+		},
+	},
+	{
+		Repository: "repo4",
+		Owner:      "owner3",
+		Languages: map[string]api.Language{
+			"golang": {
+				Bytes: 1234,
+			},
+		},
+	},
+}
+
+func TestReposHandler(t *testing.T) {
+
+	store := kvstore.NewInMemoryStore(kvstore.DefaultExpiration, kvstore.DefaultExpiration)
+
+	_ = store.Write(context.Background(), "repositories", repos, kvstore.NoExpiration)
+
+	// Create a request with the desired query parameters
+	req, err := http.NewRequest("GET", "/repositories?language=golang&owner=owner3&limit=1", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+
+	handler := ReposHandler(store)
+	err = handler(rr, req, nil)
+	if err != nil {
+		t.Fatalf("Failed to handle repos request: %v", err)
+		return
+	}
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("unexpected status code: got %v, want %v", status, http.StatusOK)
+	}
+
+	// Parse the response body and check its content
+	var responseRepositories []*api.Repository
+	err = json.Unmarshal(rr.Body.Bytes(), &responseRepositories)
+	if err != nil {
+		t.Errorf("Failed to unmarshal JSON response: %v", err)
+	}
+
+	expected := []*api.Repository{
+		{
+			Repository: "repo3",
+			Owner:      "owner3",
+			Languages: map[string]api.Language{
+				"golang": {
+					Bytes: 1234,
+				},
+			},
+		},
+	}
+
+	if !reflect.DeepEqual(responseRepositories, expected) {
+		t.Errorf("Fetched repositories do not match expected: got %+v, want %+v", responseRepositories, expected)
+	}
+}
