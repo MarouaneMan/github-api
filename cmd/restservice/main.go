fb541ff4ce93e269e545d89d59a0ebe522bb698b
Marouane <6729798+MarouaneMan@users.noreply.github.com>
2024-01-14 17:50:33 +0100

    fix: main entry point

--- cmd/restservice/main.go
+++ cmd/restservice/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/MarouaneMan/github-api/internal/config"
+	"github.com/MarouaneMan/github-api/internal/fetcher"
 	"github.com/MarouaneMan/github-api/internal/restservice"
 	"github.com/MarouaneMan/github-api/kvstore"
 	"github.com/MarouaneMan/github-api/middleware"
 	"github.com/Scalingo/go-handlers"
 	"github.com/Scalingo/go-utils/logger"
+	"github.com/go-co-op/gocron"
 	"github.com/kelseyhightower/envconfig"
 	"net/http"
 	"os"
@@ -33,14 +36,19 @@ func main() {
 
 	// Spawn fetcher job to periodically pull Github data
 	// !! Usually this block needs to run in its own process/dedicated node when using redis as a backend store
-	//{
-	//	ctx := logger.ToCtx(context.Background(), log)
-	//	cronScheduler := gocron.NewScheduler(time.UTC)
-	//	cronScheduler.Every(cfg.FetchIntervalHours).Hours().StartImmediately().Do(func() {
-	//		fetcher.Run(ctx, cfg, store)
-	//	})
-	//	cronScheduler.StartAsync()
-	//}
+	{
+		ctx := logger.ToCtx(context.Background(), log)
+		cronScheduler := gocron.NewScheduler(time.UTC)
+		cronScheduler.Every(cfg.FetchIntervalHours).Hours().StartImmediately().Do(func() {
+			// httpTransport is reused across goroutines to avoid creating too many connections
+			// we do not share the client instead to have the possibility to personalise it per use-case if needed
+			httpTransport := &http.Transport{
+				MaxConnsPerHost: 5, // do not overwhelm Github, http/2.0 takes care of concurrency
+			}
+			fetcher.Run(ctx, cfg, store, httpTransport)
+		})
+		cronScheduler.StartAsync()
+	}
 
 	log.Info("Initializing routes")
 	router := handlers.NewRouter(log)
@@ -51,7 +59,7 @@ func main() {
 
 	log = log.WithField("port", cfg.Port)
 	log.Info("Listening...")
-	err := http.ListenAndServe(fmt.Sprintf("localhost:%d", cfg.Port), router) // TODO: listen on 0.0.0.0
+	err := http.ListenAndServe(fmt.Sprintf(":%d", cfg.Port), router)
 	if err != nil {
 		log.WithError(err).Error("Fail to listen to the given port")
 		os.Exit(2)
